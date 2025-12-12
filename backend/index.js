a38d1e687f004ebd3ca88ba99b5c4be325a11698
Sathish Kumar <sathishkumar.s@mocdoc.net>
2025-12-13 02:10:02 +0530

    Folder Moved

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static('public'));
 connectDB().catch(err => { console.error(err); process.exit(1); });
 
 app.use('/api/users', require('./routes/users'));
